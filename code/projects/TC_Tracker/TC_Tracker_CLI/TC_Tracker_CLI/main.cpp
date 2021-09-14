c7b3631f820e98028eb6f6652416eaf4152e2e20
the-1000th-summer <denghao775785604@163.com>
2021-09-14 17:38:23 +0800

    add new cli project

---
+++ code/projects/TC_Tracker/TC_Tracker_CLI/TC_Tracker_CLI/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  TC_Tracker_CLI
+//
+//  Created by the-1000th-summer on 2021/9/14.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    
+    std::cout << "Hello, World!\n";
+    return 0;
+}
