93730fae3b21e919475fde7a1135c2bbc02646e2
the-1000th-summer <denghao775785604@163.com>
2021-11-01 11:26:26 +0800

    change ur2vr_cfd.cpp api for v

--- code/projects/TC_Tracker/core/src/uv2vr_cfd.cpp
+++ code/projects/TC_Tracker/core/src/uv2vr_cfd.cpp
@@ -9,7 +9,7 @@
 #include <cmath>
 #include "uv2vr_cfd.h"
 
-bool uv2vr_cfd::calRV(float *u, TTCore::TwoDArray &v, float *latData, float *lonData, int latSize, int lonSize, float msgValue, int iopt, TTCore::TwoDArray &rv) {
+bool uv2vr_cfd::calRV(float *u, float *v, float *latData, float *lonData, int latSize, int lonSize, float msgValue, int iopt, TTCore::TwoDArray &rv) {
     if (lonSize < 1 || latSize < 1) { return false; }
     if (std::abs(latData[0]) > 90.0 || std::abs(latData[latSize-1]) > 90.0) {return false;}
     
@@ -78,7 +78,7 @@ bool uv2vr_cfd::calRV(float *u, TTCore::TwoDArray &v, float *latData, float *lon
         if (lonIndex == lonSize-1) { lonIndexP1 = 0; }   // 最右边的格点
         
         for (int latIndex = 1; latIndex < latSize-1; ++latIndex) {
-            float v_lonP1 = v(latIndex,lonIndexP1), v_lonM1 = v(latIndex,lonIndexM1);
+            float v_lonP1 = v[latIndex*lonSize+lonIndexP1], v_lonM1 = v[latIndex*lonSize+lonIndexM1];
             float u_latP1 = u[(latIndex+1)*lonSize+lonIndex], u_latM1 = u[(latIndex-1)*lonSize+lonIndex];
             float u_here = u[latIndex*lonSize+lonIndex];
             if (v_lonP1 != msgValue && v_lonM1 != msgValue && u_latP1 != msgValue && u_latM1 != msgValue && u_here != msgValue) {
@@ -88,13 +88,13 @@ bool uv2vr_cfd::calRV(float *u, TTCore::TwoDArray &v, float *latData, float *lon
         
         if (jopt < 2) { continue; }
         // bottom bound (nl = 1)
-        float v_b_lonP1 = v(0,lonIndexP1), v_b_lonM1 = v(0,lonIndexM1);
+        float v_b_lonP1 = v[lonIndexP1], v_b_lonM1 = v[lonIndexM1];
         float u_b_latP1 = u[lonSize+lonIndex], u_b_here = u[lonIndex];
         if (v_b_lonP1 != msgValue && v_b_lonM1 != msgValue && u_b_latP1 != msgValue && u_b_here != msgValue) {
             rv(0,lonIndex) = (v_b_lonP1-v_b_lonM1)*dx2[0] - (u_b_latP1-u_b_here)*dyBottom + u_b_here*tLatRe[0];
         }
         // top bound (nl = nlat)
-        float v_t_lonP1 = v(latSize-1,lonIndexP1), v_t_lonM1 = v(latSize-1,lonIndexM1);
+        float v_t_lonP1 = v[(latSize-1)*lonSize+lonIndexP1], v_t_lonM1 = v[(latSize-1)*lonSize+lonIndexM1];
         float u_t_here = u[(latSize-1)*lonSize+lonIndex];
         float u_t_latM1 = u[(latSize-2)*lonSize+lonIndex];
         if (v_t_lonP1 != msgValue && v_t_lonM1 != msgValue && u_t_here != msgValue && u_t_latM1 != msgValue) {
@@ -105,14 +105,14 @@ bool uv2vr_cfd::calRV(float *u, TTCore::TwoDArray &v, float *latData, float *lon
     if (jopt == 2) {      // left/right bound?
         for (int latIndex = 1; latIndex < latSize - 1; ++latIndex) {
             // left bound (lonIndex = 0)
-            float v_l_lonP1 = v(latIndex,1), v_l_here = v(latIndex,0);
+            float v_l_lonP1 = v[latIndex*lonSize+1], v_l_here = v[latIndex*lonSize];
             float u_l_latP1 = u[(latIndex+1)*lonSize], u_l_latM1 = u[(latIndex-1)*lonSize];
             float u_l_here = u[latIndex*lonSize];
             if (v_l_lonP1 != msgValue && v_l_here != msgValue && u_l_latP1 != msgValue && u_l_latM1 != msgValue && u_l_here != msgValue) {
                 rv(latIndex,0) = (v_l_lonP1-v_l_here)*dx[latIndex] - (u_l_latP1-u_l_latM1)*dy2[latIndex] + u_l_here*tLatRe[latIndex];
             }
             // right bound (lonIndex = lonSize - 1)
-            float v_r_here = v(latIndex,lonSize-1), v_r_lonM1 = v(latIndex,lonSize-2);
+            float v_r_here = v[latIndex*lonSize+lonSize-1], v_r_lonM1 = v[latIndex*lonSize+lonSize-2];
             float u_r_latP1 = u[(latIndex+1)*lonSize+lonSize-1];
             float u_r_latM1 = u[(latIndex-1)*lonSize+lonSize-1];
             float u_r_here = u[latIndex*lonSize+lonSize-1];
