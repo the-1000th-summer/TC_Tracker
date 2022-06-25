88255e2fa4a0554f744c56efd4e61bac2f52fd3c
the-1000th-summer <denghao775785604@163.com>
2022-06-25 17:56:29 +0800

    add macos project

---
+++ code/projects/TC_Tracker/TC_Tracker_mac/TC_Tracker_mac/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  TC_Tracker_mac
+//
+//  Created by 邓浩 on 2022/6/25.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
